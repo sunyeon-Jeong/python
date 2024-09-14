efa7eef604752de3ad0583b9b2bc9a154c79cf59
sunyeon-Jeong <sunyeon4012@gmail.com>
2024-09-14 12:00:17 +0900

    Feat : 파이썬입출력(04.function) #3 - 함수

--- 04.function/function.py
+++ 04.function/function.py
@@ -184,3 +184,16 @@ def vartest():
     a = a+1
 vartest()
 print(a)
+
+# lambda 예약어
+'''
+- lambda : 함수를 생성할 때 사용하는 예약어 (= def)
+- 함수_이름 = lambda 매개변수1, 매개변수2 : 매개변수를_이용한_표현식
+'''
+add = lambda a, b: a+b
+result = add(3, 4)
+print(result)
+
+mul = lambda a, b: a*b
+result = mul(3, 4)
+print(result)
